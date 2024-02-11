9fd90d43fe397ebbe421f7d8b5e2d54a37763807
AnirudhPI <anirudhinamdar@gmail.com>
2024-02-10 20:29:58 -0700

    General kafka client updated with prediction API

--- flask-server/tensorflow-module.py
+++ flask-server/tensorflow-module.py
@@ -3,7 +3,7 @@ from json import loads
 from flask import Flask
 from flask import request
 from flask.wrappers import Response
-from kafkaproducer import Kafkaproducer
+from kafkaclient import KafkaClient
 from flask_cors import CORS
 import requests
 import numpy as np
@@ -11,10 +11,31 @@ import numpy as np
 app = Flask(__name__)
 CORS(app)
 
-OUTPUT_LEN = 6  # Assuming this is the number of possible classes for quality
+OUTPUT_LEN = 6
 TOPIC_NAME = "ml-model-train"
 
-producer = Kafkaproducer()
+producer = KafkaClient(TOPIC_NAME)
+
+@app.route("/")
+def default():
+    return "The Flask app is Working"
+
+# for predictions
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    print("Running prediction")
+    print("Request Data:")
+    
+    data = loads(request.data.decode(encoding="ascii"))
+    print(data)
+
+    # construct payload for TensorFlow serving
+    raw = "{\"instances\":[["
+    for key, value in data.items():
+        raw = raw + data[key] + ","
+    raw = raw[:-1]  # Remove trailing comma
+    raw = raw + "]]}"
+    print(raw)
 
 
 if __name__ == "__main__":
